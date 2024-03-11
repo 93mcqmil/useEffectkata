5a80fb4becad36566e0145a08f30023c5a93f357
93mcqmil <mcqueen_1993@hotmail.com>
2024-03-11 17:06:49 +0100

    fetching data inside a useEffect function

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Person from "./Components/Person";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Star Wars</h1>
+      <Person />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Person.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+type SWPerson = {
+  name: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+};
+
+const Person = () => {
+  const [person, setPerson] = useState({} as SWPerson);
+
+  useEffect(() => {
+    const url = "https://swapi.py4e.com/api/people/1/";
+    const fetchLuke = async () => {
+      const result = await fetch(url); // result kan heta respons
+      const luke = await result.json(); // luke kan heta data
+      if (!ignore) {
+        setPerson({
+          name: luke.name,
+          eye_color: luke.eye_color,
+          birth_year: luke.birth_year,
+          gender: luke.gender,
+        });
+      }
+    };
+    let ignore = false;
+    fetchLuke();
+    return () => {
+      ignore = true;
+    };
+  }, []); //  Tom array indikerar att effekten körs endast vid första renderingen
+
+  return (
+    <div>
+      {/* Name: {""} */}
+      {person
+        ? `Name: ${person.name}, Eyes: ${person.eye_color}, Birth year: ${person.birth_year}, Gender: ${person.gender}`
+        : "Loading..."}
+    </div>
+  );
+};
+export default Person;
